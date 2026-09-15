6ffe7c014651afc74212a6f26c289395bfd196cb
Chanuka Fernando <roshelchanuka@gmail.com>
2026-09-15 15:04:57 +0530

    fix: update Content Security Policy to include additional sources for scripts, images, and connections

--- next.config.mjs
+++ next.config.mjs
@@ -26,7 +26,7 @@ const securityHeaders = [
   },
   {
     key: 'Content-Security-Policy',
-    value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https://drive.google.com https://lh3.googleusercontent.com https://i.ibb.co https://res.cloudinary.com https://flagcdn.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://drive.google.com; media-src 'self' blob: data: https://drive.google.com;"
+    value: "default-src 'self'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://unpkg.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; img-src 'self' blob: data: https://drive.google.com https://lh3.googleusercontent.com https://i.ibb.co https://res.cloudinary.com https://flagcdn.com https://avatars.githubusercontent.com; font-src 'self' data: https://fonts.gstatic.com; connect-src 'self' https://drive.google.com https://api.github.com; media-src 'self' blob: data: https://drive.google.com;"
   }
 ];
 
